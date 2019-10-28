8acfc01f60d463cf9edf728fa6ee97cf095d1f9e
Joana Faustino <joanarozendo@gmail.com>
2019-10-28 23:27:31 +0000

    initial commit

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,31 +1,40 @@
 class Chronometer {
   constructor() {
-    // ..
+    this.currentTime = 0;
+    this.intervalId;
   }
 
-  // startClick() {
-  // ..
-  // }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
 
-  // getMinutes() {
-  // ..
-  // }
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
 
-  // getSeconds() {
-  // ..
-  // }
+  getSeconds() {
+    return this.currentTime % 60 ;
+  }
 
-  // twoDigitsNumber() {
-  // ..
-  // }
+  twoDigitsNumber(num) {
+    if (num.toString().length === 1) {
+      return '0' + num.toString();
+    } else if (num.toString().length === 2) {
+      return num.toString();
+    } else {
+      return '00';
+    }
+  }
 
-  // stopClick() {
-  // ..
-  // }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
 
-  // resetClick() {
-  // ..
-  // }
+  resetClick() {
+  
+  }
 
   // splitClick() {
   // ..
